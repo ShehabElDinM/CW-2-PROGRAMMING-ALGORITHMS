010eeb38b2b772a51fcbff1a9238b7992b0b6bd0
ShehabElDin <SE2101590@tkh.edu.eg>
2024-04-21 12:43:56 +0200

    Updated CW2-Client.cpp
    Added Authentication function and Receive function

--- CW2-Client.cpp
+++ CW2-Client.cpp
@@ -31,3 +31,42 @@ string Encrypt(const string& message, int shift) {
 string Decrypt(const string& message, int shift) {
     return Encrypt(message, 26 - shift);
 }
+
+bool Auth(int server_socket) {
+    char username[1024];
+    char password[1024];
+
+    cout << "Enter username: ";
+    cin.getline(username, sizeof(username));
+
+    cout << "Enter password: ";
+    cin.getline(password, sizeof(password));
+
+    string credentials = string(username) + " " + string(password);
+
+    string encrypted_credentials = Encrypt(credentials, 3);
+
+    if (send(server_socket, encrypted_credentials.c_str(), encrypted_credentials.length(), 0) == -1) {
+        log("Send failed");
+        return false;
+    }
+
+    return true;
+}
+
+void* Receive(void* arg) {
+    int server_socket = *((int*)arg);
+    char buffer[1024] = {0};
+    while (true) {
+        int valread = recv(server_socket, buffer, sizeof(buffer), 0);
+        if (valread <= 0) {
+            log("Receive failed");
+            break;
+        }
+        string decrypted_message = Decrypt(buffer, 3);
+        cout << "server: " << decrypted_message << endl;
+
+        memset(buffer, 0, sizeof(buffer));
+    }
+    return NULL;
+}
